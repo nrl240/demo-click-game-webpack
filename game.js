95acc9eb67470ac55527d2e2114ce700b527683f
Noelle Laureano <nrlaureano@gmail.com>
2019-11-17 18:36:06 -0500

    init

---
+++ game.js
@@ -0,0 +1,21 @@
+
+let numTimesClicked = 0
+
+function win() {
+  alert('You win!')
+  reset()
+}
+
+function reset() {
+  numTimesClicked = 0
+}
+
+function click() {
+  numTimesClicked++
+  console.log('numTimesClicked -->', numTimesClicked)
+  if (numTimesClicked === 10) {
+    win()
+  }
+}
+
+module.exports = click
---
+++ main.js
@@ -0,0 +1,15 @@
+const click = require('./game')
+
+const clicker = document.getElementById('clicker')
+
+clicker.addEventListener('click', function(e) {
+  console.log('click')
+  click()
+})
+
+setInterval(function() {
+  let x = Math.random() * 700
+  let y = Math.random() * 700
+  clicker.style.left = `${x}px`
+  clicker.style.top = `${y}px`
+}, 1000)
